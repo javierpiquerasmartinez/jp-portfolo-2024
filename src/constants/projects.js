cf1703b6908831f73fa47373cf0e702c90230b4c
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2024-01-08 16:48:15 +0100

    AdventJS new file and some refactoring

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -14,7 +14,7 @@ export const PROJECTS = [
       name: "AdventJS 2023",
       description:
         "Repository containing my solutions for all the AdventJS 2023 challenge. AdventJS is a challenge created by midudev where developers can upload their JavaScript / TypeScript solutions for each challenge, in order to improve their skills. This challenge takes from the 1st to the 25th of December.",
-      img: "https://github.com/javierpiquerasmartinez/adventjs-2023/raw/main/static/adventjs-logo.png",
+      img: "/adventJS.png",
       github: "https://github.com/javierpiquerasmartinez/adventjs-2023",
       link: "",
       tags: [ TAGS.JAVASCRIPT, TAGS.NODEJS ]
