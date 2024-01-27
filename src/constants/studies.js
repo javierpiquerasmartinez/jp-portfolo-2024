4fdc2d6bb3de8acdb6ec48d547cef600cf3166b6
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2024-01-28 00:30:20 +0100

    Studies section

---
+++ src/constants/studies.js
@@ -0,0 +1,17 @@
+export const STUDIES = [
+  {
+    title: 'Computer Engineering Degreee',
+    institution: 'UPV - Universidad Politècnica de València',
+    date: '2013-2017'
+  },
+  {
+    title: 'Masters Degree in Computer Engineering',
+    institution: 'UCM - Universidad Complutense de Madrid',
+    date: '2017 - 2019'
+  },
+  {
+    title: 'Masters Degree in Engineering and Data Science',
+    institution: 'UNED - Universidad Nacional Española a Distancia',
+    date: '2020 - 2022'
+  },
+];
