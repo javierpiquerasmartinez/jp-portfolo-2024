4b4d3c8dd53a3034929795e15805cffd4ab1929c
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2024-03-26 15:00:29 +0100

    Email service implementation

---
+++ src/services/email.js
@@ -0,0 +1,28 @@
+import { Resend } from 'resend';
+
+const resend = new Resend('re_BnLTv3t8_4nfqqSdQLZ4Wz3wizXrceyTP');
+
+function sendEmail(name, address, message) {
+  sendEmailToUser(name, address, message)
+  sendEmailToJavier(name, address, message)
+}
+
+function sendEmailToUser(name, address, message) {
+  resend.emails.send({
+    from: 'Javier Piqueras <hello@javierpiqueras.com>',
+    to: address,
+    subject: 'Your email to Javier Piqueras has been sent successfully',
+    html: `<h1>Thanks for contacting me</h1><div>Hello ${name}. Your email to Javier Piqueras has been sent succcessfully, you will receive soon a response.</div><div>A copy of your email:</div><div>${message}</div>`
+  })
+}
+
+function sendEmailToJavier(name, address, message) {
+  resend.emails.send({
+    from: `${name} <portfolio@javierpiqueras.com>`,
+    to: 'javierpiquerasmartinez@gmail.com',
+    subject: `Email from ${name}`,
+    html: `<p>Name: ${name}</p><p>Email: ${address}</p><p>Message: ${message}</p>`
+  })
+}
+
+sendEmail('Kenza El Alaoui', 'javierpiquerasmartinez@gmail.com', 'Hello Javier, I am interested in your portfolio. Best Regards. TES')
